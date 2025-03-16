e2ecb010732781c4a3a95103155b61bc70c44de7
selosele <osh3384@naver.com>
2025-03-16 19:13:21 +0900

    테이블 캡션 생성기능 개발

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Configuration;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HtmlAgilityPack;
 
 namespace HtmlTableCaptionMaker;
 
@@ -21,9 +23,38 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private void Button_Click(object sender, RoutedEventArgs e)
+    /// <summary>
+    /// 캡션생성 버튼을 클릭한다.
+    /// </summary>
+    private void MakeCaptionButton_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("테스트", "알림");
-        makeCaptionBtn.Content = "test";
+        var html = inputTextBox.Text;
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+
+        var table = doc.DocumentNode.SelectSingleNode("//table");
+        var thead = table.SelectSingleNode("./thead");
+        var tr = thead.SelectNodes("./tr");
+        List<string> thTextList = [];
+        for (int i = 0; i < tr.Count; i++)
+        {
+            var th = tr[i].SelectNodes("./th");
+            for (int j = 0; j < th.Count; j++)
+            {
+                thTextList.Add(th[j].InnerText.Trim());
+            }
+        }
+
+        var outputText = "";
+        if (string.IsNullOrEmpty(tableTitleTextBox.Text))
+        {
+            outputText = $"<caption>{string.Join(", ", thTextList)}</caption>";
+        }
+        else
+        {
+            outputText = $"<caption>{tableTitleTextBox.Text} - {string.Join(", ", thTextList)}</caption>";
+        }
+
+        outputTextBox.Text = outputText;
     }
 }
